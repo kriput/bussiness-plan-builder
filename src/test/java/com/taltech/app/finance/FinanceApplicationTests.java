22bc894ed9e8fcbb66a8844c1ae268fc2b429031
Kristofer Putsinin <kristofer.putsinin@transferwise.com>
2024-02-19 14:59:21 +0200

    initial commit

---
+++ src/test/java/com/taltech/app/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taltech.app.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
