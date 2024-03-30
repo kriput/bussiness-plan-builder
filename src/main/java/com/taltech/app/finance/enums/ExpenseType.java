1d6d79400eef7efdd1304170dc9a6a2b0e20da19
Kristofer Putsinin <kristofer.putsinin@transferwise.com>
2024-03-30 15:23:36 +0200

    data seeding change

--- src/main/java/com/taltech/app/finance/enums/ExpenseType.java
+++ src/main/java/com/taltech/app/finance/enums/ExpenseType.java
@@ -1,5 +1,35 @@
 package com.taltech.app.finance.enums;
 
 public enum ExpenseType {
-    WORKFORCE
+    // Production
+    RAW_MATERIALS,
+    OUTSOURCED_SERVICES,
+
+    // Marketing
+    MARKETING,
+
+    // Workspace
+    RENT,
+    UTILITY,
+    OTHER_WORKSPACE_COSTS,
+
+    // Transport
+    TRANSPORT_COMPENSATION,
+    OTHER_TRANSPORT_COSTS,
+
+    // IT
+    IT,
+
+    // Other
+    OTHER_COSTS,
+
+    // Workforce
+    SALARY,
+    SOCIAL_TAX,
+    UNEMPLOYMENT_INSURANCE_TAX,
+    TRAINING_COSTS,
+
+    // Other taxes
+    OTHER_TAXES,
+    VAT
 }
